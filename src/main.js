20f9309f593e34ce0209cccab60f377ba939b516
Nina Przygoda <nprzygoda@student.agh.edu.pl>
2026-05-28 21:29:42 +0200

    test

--- src/main.js
+++ src/main.js
@@ -34,7 +34,6 @@ displayArticles();
 
 const formularz = document.getElementById('formularz')
 formularz.addEventListener("submit", (event) => {
-    event.preventDefault();
     const data = {
     title: document.getElementById('tytul').value,
     subtitle: document.getElementById('podtytul').value,
