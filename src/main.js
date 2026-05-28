f232c98ce07b7c47ba7bab6174b3e7184a8651b1
Nina Przygoda <nprzygoda@student.agh.edu.pl>
2026-05-28 11:09:45 +0200

    fetch proba

--- src/main.js
+++ src/main.js
@@ -1,32 +1,6 @@
-const fetchArticles = async () => {
- try {
- const response = await fetch(
- "https://piwvqmsaiayahfjipzet.supabase.co/rest/v1/", {
+const response = fetch('https://piwvqmsaiayahfjipzet.supabase.co/rest/v1/', {
  headers: {
- apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpd3ZxbXNhaWF5YWhmamlwemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MTc3MzQsImV4cCI6MjA5NTI5MzczNH0.yy6TuHrE30QWtyzfbG0P4OZZ0PYttm4wQBd15KrZvxg",
+ apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpd3ZxbXNhaWF5YWhmamlwemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MTc3MzQsImV4cCI6MjA5NTI5MzczNH0.yy6TuHrE30QWtyzfbG0P4OZZ0PYttm4wQBd15KrZvxg',
  },
- });
- const data = await response.json();
- console.log(data);
- return data;
- } catch (error) {
- console.error('Fetch error:', error);
- }
-};
-
-const displayArticles = async () => {
-    const artykuly = await fetchArticles();
-    const wyswietlanie = document.getElementById('wyswietlanie');
-    artykuly.forEach(artykul => {
-        const wa = document.createElement('div');
-        wa.innerHTML = `
-        <h1>${artykul.title}</h1>
-        <h2>${artykul.subtitle}</h2>
-        <h3>${artykul.author}</h3>
-        <h4>${artykul.created_at}</h4>
-        <p>${artykul.content}</p>
-        `;
-        wyswietlanie.appendChild(wa);
-    });
-};
-displayArticles();
+});
+console.log(response);
