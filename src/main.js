2c51f85c9c67a22ceaca16925f45fa5667f93407
Nina Przygoda <nprzygoda@student.agh.edu.pl>
2026-05-28 10:49:28 +0200

    fetch 2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const fetchArticles = async () => {
  const response = await fetch(
  "https://piwvqmsaiayahfjipzet.supabase.co/rest/v1/", {
  headers: {
- apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpd3ZxbXNhaWF5YWhmamlwemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MTc3MzQsImV4cCI6MjA5NTI5MzczNH0.yy6TuHrE30QWtyzfbG0P4OZZ0PYttm4wQBd15KrZvxg",
+ apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpd3ZxbXNhaWF5YWhmamlwemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MTc3MzQsImV4cCI6MjA5NTI5MzczNH0.yy6TuHrE30QWtyzfbG0P4OZZ0PYttm4wQBd15KrZvxg",
  },
  });
  const data = await response.json();
