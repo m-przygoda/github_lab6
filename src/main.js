92bbb178c7abef76edfe0476c9e4fea77f5ef9f8
Nina Przygoda <nprzygoda@student.agh.edu.pl>
2026-05-28 21:25:30 +0200

    Form 3

--- src/main.js
+++ src/main.js
@@ -43,10 +43,10 @@ formularz.addEventListener("submit", (event) => {
     };
     const createNewArticle = async (data) => {
  try {
-    const response = await fetch("api url", {
+    const response = await fetch("https://piwvqmsaiayahfjipzet.supabase.co/rest/v1/article?select=*", {
         method: 'POST',
         headers: {
-            apiKey: "api key",
+            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpd3ZxbXNhaWF5YWhmamlwemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MTc3MzQsImV4cCI6MjA5NTI5MzczNH0.yy6TuHrE30QWtyzfbG0P4OZZ0PYttm4wQBd15KrZvxg",
             'Content-Type' : 'application/json' ,
         },
         body: JSON.stringify(data),
